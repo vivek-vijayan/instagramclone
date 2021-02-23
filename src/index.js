fc35b444e6a896a9da7bda6d6e427c938c0710b8
Vivek Vijayan <vivekvijayan3@icloud.com>
2021-02-24 02:37:15 +0530

    AppHeader Update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import AppHeader from './App-Header/AppHeader'
+
+import AppHeader from './AppHeader/AppHeader'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
 
 ReactDOM.render(<AppHeader/>, document.getElementById("header"))
 
