1d226b01d3540d76026a918f01fc9a70edf02485
Vivek Vijayan <vivekvijayan3@icloud.com>
2021-02-25 03:18:27 +0530

    Updated UI

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AppHeader from './AppHeader/AppHeader'
+
+function App() {
+  return (
+    <div>
+      <AppHeader></AppHeader>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-import AppHeader from './AppHeader/AppHeader'
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-
-ReactDOM.render(<AppHeader/>, document.getElementById("header"))
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
